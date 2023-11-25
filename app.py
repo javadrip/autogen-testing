f4043057ce3551aae74eaa65ec298495e1ca3876
javadrip <javadripdev@gmail.com>
2023-11-25 15:53:07 +0800

    Basic local set up complete

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ import autogen
 
 config_list = [
     {
-        # 'model': 'gpt-3.5-turbo-16k',
-        "api_type": "open_ai",
-        "api_base": "http://localhost:1234/v1",
+        # "model": "gpt-3.5-turbo-16k",
+        # "api_type": "open_ai",
+        "base_url": "http://localhost:1234/v1",
         "api_key": "NULL",  # NULL because running code locally
     }
 ]
 
 llm_config = {
-    "request_timeout": 600,
+    "timeout": 600,
     "seed": 42,
     "config_list": config_list,
     "temperature": 0,
@@ -27,12 +27,13 @@ llm_config = {
 
 assistant = autogen.AssistantAgent(
     name="assistant",
+    system_message="You are a creative copywriter who works in Ogilvy. Reply `TERMINATE` in the end when everything is done.",
     llm_config=llm_config,
 )
 
 user_proxy = autogen.UserProxyAgent(
     name="user_proxy",
-    human_input_mode="TERMINATE",
+    human_input_mode="ALWAYS",  # Possible values: "ALWAYS", "TERMINATE", "NEVER".
     max_consecutive_auto_reply=10,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
     code_execution_config={"work_dir": "web"},
@@ -42,7 +43,7 @@ user_proxy = autogen.UserProxyAgent(
 )
 
 task = """
-Give me a summary of this article: https://en.wikipedia.org/wiki/Artificial_intelligence
+Write a creative Kentucky Fried Chicken advertisement targeting vegans who are into the fit and healthy lifestyle.
 """
 
 user_proxy.initiate_chat(assistant, message=task)
